ae865d7c2f7d7504958a8d6422cd5fb0f3763ab3
Bill <wherrera10@users.noreply.github.com>
2024-12-02 12:33:40 -1000

    Add files via upload

---
+++ src/day01.jl
@@ -0,0 +1,12 @@
+using BenchmarkTools
+
+const DIR = "C:/Users/wherr/OneDrive/Documents/Julia Programs/aoc_2024"
+
+function day01()
+    part = [0, 0]
+    numbers = parse.(Int, split(read("$DIR/day01.txt", String), r"\s+"))
+    a, b = sort!(numbers[1:2:length(numbers)]), sort!(numbers[2:2:length(numbers)])
+    part .= sum(abs.(a .- b)), sum(a[i] * count(==(a[i]), b) for i in eachindex(a))
+end
+
+@show day01() # 54940, 54208
---
+++ src/day02.jl
@@ -0,0 +1,26 @@
+using BenchmarkTools
+
+const DIR = "C:/Users/wherr/OneDrive/Documents/Julia Programs/aoc_2024"
+
+function day02()
+    part = [0, 0]
+    reports = [parse.(Int, split(line, r"\s+")) for line in readlines("$DIR/day02.txt")]
+    for report in reports
+        d = diff(report)
+        if all(n -> 1 <= n <= 3, d) || all(n -> -3 <= n <= -1, d)
+            part[1] += 1
+            part[2] += 1
+        else
+            for i in eachindex(report)
+                d2 = diff(deleteat!(copy(report), i))
+                if all(n -> 1 <= n <= 3, d2) || all(n -> -3 <= n <= -1, d2)
+                    part[2] += 1
+                    break
+                end
+            end
+        end
+    end
+    return part
+end
+
+@show day02() # 54940, 54208
