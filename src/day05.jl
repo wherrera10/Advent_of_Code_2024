32b10f1d3f77a548926a7b52f57eb06ae1915999
Bill <wherrera10@users.noreply.github.com>
2024-12-05 07:14:53 -1000

    Update day05.jl

--- src/day05.jl
+++ src/day05.jl
@@ -8,8 +8,8 @@ function day05()
 	rules = [parse.(Int, split(line, "|")) for line in split(rule_text, "\n")]
 	updates = [parse.(Int, split(line, ",")) for line in split(update_text, "\n")]
 
-	function is_sorted(update)
-		for rule in rules
+	function is_sorted(update, rule_set)
+		for rule in rule_set
 			p1 = findfirst(==(rule[1]), update)
 			if !isnothing(p1)
 				p2 = findfirst(==(rule[2]), update)
@@ -22,11 +22,11 @@ function day05()
 	end
 
 	for a in updates
-        if is_sorted(a)
+        if is_sorted(a, rules)
     		part[1] += a[length(a)÷2+1]
         else
             apply = filter(r -> r[1] in a && r[2] in a, rules)
-            while !is_sorted(a)
+            while !is_sorted(a, apply)
                 for rule in apply
                     while (x = findfirst(==(rule[1]), a)) > findfirst(==(rule[2]), a)
                         a[x], a[x-1] = a[x-1], a[x]
