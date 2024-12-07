15752cd464f424e627541c33acf1d0cb604f4915
Bill <wherrera10@users.noreply.github.com>
2024-12-07 11:54:24 -1000

    Update day01.jl

--- src/day01.jl
+++ src/day01.jl
@@ -1,10 +1,9 @@
 const DIR = "aoc_2024"
 
 function day01()
-    part = [0, 0]
     numbers = parse.(Int, split(read("$DIR/day01.txt", String), r"\s+"))
     a, b = sort!(numbers[1:2:length(numbers)]), sort!(numbers[2:2:length(numbers)])
-    part .= sum(abs.(a .- b)), sum(a[i] * count(==(a[i]), b) for i in eachindex(a))
+    sum(abs.(a .- b)), sum(a[i] * count(==(a[i]), b) for i in eachindex(a))
 end
 
 @show day01() 
