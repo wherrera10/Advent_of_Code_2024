1818dd1b57df7af1b3b3ccb7477fd85d3a86dd20
Bill <wherrera10@users.noreply.github.com>
2024-12-08 07:44:16 -1000

    Update day08.jl

--- src/day08.jl
+++ src/day08.jl
@@ -1,8 +1,6 @@
 const DIR = "aoc_2024"
 
 function day08()
-    part = [0, 0]
-
     grid = stack((collect(line) for line in split(read("$DIR/day08.txt", String), "\n")), dims = 1)
     rows, cols = size(grid)
     delta(p1, p2) = [p2[1] - p1[1], p2[2] - p1[2]]
@@ -13,27 +11,6 @@ function day08()
         push!(nodes, [p1[1] - d[1], p1[2] - d[2]], [p2[1] + d[1], p2[2] + d[2]])
         return filter(n -> 1 <= n[1] <= rows && 1 <= n[2] <= cols, nodes)
     end
-    
-    frequencies = Dict{Char, Vector{Vector{Int}}}()
-    for p in CartesianIndices(grid)
-        if grid[p] != '.'
-            if !haskey(frequencies, grid[p])
-                frequencies[grid[p]] = Vector{Vector{Int}}()
-            end
-            push!(frequencies[grid[p]], [p[1], p[2]])
-        end
-    end
-
-    locations = Vector{Int}[]
-    for pos in values(frequencies)
-        for i in eachindex(pos)
-            for j in i+1:length(pos)
-                nodes = antinodes(pos[i], pos[j])
-                append!(locations, nodes)
-            end
-        end
-    end
-    part[1] = length(unique(locations))
 
     function updated_rules(p1, p2)
         nodes = Vector{Vector{Int}}()
@@ -52,18 +29,29 @@ function day08()
         return nodes
     end
 
+    frequencies = Dict{Char, Vector{Vector{Int}}}()
+    for p in CartesianIndices(grid)
+        if grid[p] != '.'
+            if !haskey(frequencies, grid[p])
+                frequencies[grid[p]] = Vector{Vector{Int}}()
+            end
+            push!(frequencies[grid[p]], [p[1], p[2]])
+        end
+    end
+
+    locations = Vector{Int}[]
     updated = Vector{Int}[]
     for pos in values(frequencies)
         for i in eachindex(pos)
             for j in i+1:length(pos)
+                nodes = antinodes(pos[i], pos[j])
+                append!(locations, nodes)
                 nodes = updated_rules(pos[i], pos[j])
                 append!(updated, nodes)
             end
         end
     end
-    part[2] = length(unique(updated))
-    
-    return part
+    length(unique(locations)), length(unique(updated))
 end
 
-@show day08() # [361, 1249]
+@show day08() # (361, 1249)
