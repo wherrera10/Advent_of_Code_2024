b16790fa1394674098a8c2b213fd780085aa6d7b
Bill <wherrera10@users.noreply.github.com>
2024-12-09 21:39:03 -1000

    Update day10.jl

--- src/day10.jl
+++ src/day10.jl
@@ -3,17 +3,17 @@ using BenchmarkTools
 const DIR = "aoc_2024"
 
 function day10()
-	part = [0, 0]
-	mat = parse.(Int8, stack([collect(line) for line in readlines("$DIR/day10.txt")], dims = 1))
+    part = [0, 0]
+    mat = parse.(Int8, stack([collect(line) for line in readlines("$DIR/day10.txt")], dims = 1))
     rows, cols = size(mat)
     trailheads = collect(findall(==(0), mat))
-    options = [[0,0], [0,0], [0,0],[0,0]]
+    options = [[0, 0], [0, 0], [0, 0], [0, 0]]
     for t in trailheads
         trails = [[t[1], t[2]]]
         new_trails = empty(trails)
         for wanted in 1:9
             for (x, y) in trails
-                options .= [[x - 1, y], [x, y +1], [x+1, y], [x, y-1]]
+                options .= [[x - 1, y], [x, y + 1], [x + 1, y], [x, y - 1]]
                 append!(new_trails, filter(c -> 1 <= c[1] <= rows && 1 <= c[2] <= cols && mat[c[1], c[2]] == wanted, options))
             end
             trails, new_trails = new_trails, trails
