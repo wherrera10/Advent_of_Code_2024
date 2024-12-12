288830c1a3074721008319f9797ac1f2cdd16b08
Bill <wherrera10@users.noreply.github.com>
2024-12-11 18:30:20 -1000

    Update day11.jl

--- src/day11.jl
+++ src/day11.jl
@@ -1,7 +1,5 @@
 const DIR = "aoc_2024"
 const MAX_BLINKS = 75
-const CACHE_SIZE = MAX_BLINKS * 10_000_000
-const day11_int_cache = zeros(Int, CACHE_SIZE)
 const day11_cache = Dict{Int, Int}()
 
 function day11_blink(stone, n)
@@ -9,21 +7,14 @@ function day11_blink(stone, n)
     n -= 1
     iszero(stone) && return day11_blink(1, n)
     key = stone * MAX_BLINKS + n
-    if key <= CACHE_SIZE && (cached = day11_int_cache[key] > 0)
-        return cached
-    end
-    if (cached = get(day11_cache[key], stone, -1)) != -1
-        return cached 
+    if haskey(day11_cache, key)
+        return day11_cache[key]
     end
     d = ndigits(stone)
-    len = iseven(d) ? 
+    len = iseven(d) ?
           (i = 10^(d ÷ 2); day11_blink(stone ÷ i, n) + day11_blink(stone % i, n)) :
           day11_blink(stone * 2024, n)
-    if key <= CACHE_SIZE
-        day11_int_cache[key] = len
-    else
-        day11_cache[key] = len
-    end
+    day11_cache[key] = len
     return len
 end
 
