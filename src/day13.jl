48121572208279ca7a57444e7f57eefde795ac94
Bill <wherrera10@users.noreply.github.com>
2024-12-13 15:28:38 -1000

    Add files via upload

---
+++ src/day13.jl
@@ -0,0 +1,43 @@
+using BenchmarkTools
+
+import LinearAlgebra.det
+
+const DIR = "C:/Users/wherr/OneDrive/Documents/Julia Programs/aoc_2024"
+
+function day13()
+    part = [0, 0]
+    text = read("$DIR/day13.txt", String)
+    feasible = 0
+    machines = split(text, "\n\n")
+    regex = r"\D+(\d+)\D+(\d+)"
+    for m in machines
+        a, b, p = split(m, "\n")
+        ax, ay = parse.(Int, match(regex, a).captures)
+        bx, by = parse.(Int, match(regex, b).captures)
+        px, py = parse.(Int, match(regex, p).captures)
+
+        A = [ax bx; ay by]
+        if abs(det(A)) > 0.01
+            a, b = round.(A \ [px, py])
+            if ax * a + bx * b == px && ay * a + by * b == py
+                part[1] += 3a + b
+            end
+        end
+        part_2_scalar = 10000000000000
+        p2x, p2y = (px, py) .+ part_2_scalar
+        A = [ax bx; ay by]
+        if abs(det(A)) > 0.01
+            a, b = round.(A \ [p2x, p2y])
+            if ax * a + bx * b == p2x && ay * a + by * b == p2y
+                part[2] += 3a + b
+            end
+        end
+    end
+
+    return part
+end
+
+@btime day13()
+
+@show day13() #  [38714, 74015623345775]
+
