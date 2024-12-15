31f417cd862b01ccd88a97fffe015c269ae6cfc9
Bill <wherrera10@users.noreply.github.com>
2024-12-15 06:41:21 -1000

    Update day15.jl

--- src/day15.jl
+++ src/day15.jl
@@ -11,62 +11,12 @@ function day15()
     moves = map(ch -> findfirst(==(ch), "^>v<"), filter(c -> c in "^>v<", collect(move_chars)))
 
     score(p) = 100 * (p[1] - 1) + p[2] - 1
-    total_score() = sum(map(score, filter(c -> mat[c] == 'O', CartesianIndices(mat))))
-
-    function step(pos, mov)
-        d = directions[mov]
-        next_pos = CartesianIndex(pos[1] + d[1], pos[2] + d[2])
-        !(1 <= next_pos[1] <= rows && 1 <= next_pos[2] <= cols) && return pos
-        mat[next_pos] == '#' && return pos
-        if mat[next_pos] == '.'
-            mat[pos] = '.'
-            pos = next_pos
-            mat[pos] = '@'
-        elseif mat[next_pos] == 'O'
-            tmp = deepcopy(next_pos)
-            while true
-                tmp = CartesianIndex(tmp[1] + d[1], tmp[2] + d[2])
-                !(1 <= tmp[1] <= rows && 1 <= tmp[2] <= cols) && break
-                (mat[tmp] == '#' || mat[tmp] == '@') && break
-                if mat[tmp] == '.'
-                    if mov == 1
-                        for x in next_pos[1]:-1:tmp[1]
-                            mat[x, tmp[2]] = 'O'
-                        end
-                    elseif mov == 2
-                        for y in next_pos[2]:tmp[2]
-                            mat[tmp[1], y] = 'O'
-                        end
-                    elseif mov == 3
-                        for x in next_pos[1]:tmp[1]
-                            mat[x, tmp[2]] = 'O'
-                        end
-                    else
-                        for y in next_pos[2]:-1:tmp[2]
-                            mat[tmp[1], y] = 'O'
-                        end
-                    end
-                    mat[pos] = '.'
-                    mat[next_pos] = '@'
-                    pos = next_pos
-                    break
-                end
-            end
-        end # do nothing if mat[next_pos] == '#'
-        return pos
-    end
-
-    pos = findfirst(==('@'), mat)
-    for i in eachindex(moves)
-        mov = moves[i]
-        pos = step(pos, mov)
-    end
-    part[1] = total_score()
+    total_score() = sum(map(score, filter(c -> mat[c] ∈ "[O", CartesianIndices(mat))))
 
     function can_move_row(p, mov)
         indexes = [p]
         d = directions[mov]
-        if mov ∈ [1, 3] # north, south
+        if mat[p] != 'O' && mov ∈ [1, 3] # north, south
             if mat[p] == '['
                 push!(indexes, CartesianIndex(p[1], p[2] + 1))
             elseif mat[p] == ']'
@@ -96,7 +46,7 @@ function day15()
         return sort!(unique(indexes), rev = mov ∈ [2, 3])
     end
 
-    function step2(pos, mov)
+    function step(pos, mov)
         d = directions[mov]
         next_pos = CartesianIndex(pos[1] + d[1], pos[2] + d[2])
         !(1 <= next_pos[1] <= rows && 1 <= next_pos[2] <= cols) && return pos
@@ -105,7 +55,7 @@ function day15()
             mat[pos] = '.'
             pos = next_pos
             mat[pos] = '@'
-        elseif mat[next_pos] in "[]"
+        elseif mat[next_pos] in "[]O"
             a = can_move(next_pos, mov)
             if !isempty(a)
                 for c in a
@@ -120,15 +70,12 @@ function day15()
         return pos
     end
 
-    function total_score2()
-        total = 0
-        for y in 1:cols, x in 1:rows
-            if mat[x, y] == '['
-                total += 100 * (x - 1) + y - 1
-            end
-        end
-        return total
+    pos = findfirst(==('@'), mat)
+    for i in eachindex(moves)
+        mov = moves[i]
+        pos = step(pos, mov)
     end
+    part[1] = total_score()
 
     cols *= 2
     mat = fill('.', rows, cols)
@@ -140,12 +87,13 @@ function day15()
             mat[x, y] = s == 'O' ? ']' : s
         end
     end
+
     pos = findfirst(==('@'), mat)
     for i in eachindex(moves)
         mov = moves[i]
-        pos = step2(pos, mov)
+        pos = step(pos, mov)
     end
-    part[2] = total_score2()
+    part[2] = total_score()
 
     return part
 end
