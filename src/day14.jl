e4743a60b4d6ea76aef71b6cd07ef322908b3c79
Bill <wherrera10@users.noreply.github.com>
2024-12-14 15:48:07 -1000

    Update day14.jl

--- src/day14.jl
+++ src/day14.jl
@@ -1,46 +1,51 @@
 const DIR = "aoc_2024"
 
 function day14()
-	part = [0, 0]
+    part = [0, 0]
 
-	regex = r"[^\d-]+([\d-]+)[^\d-]+([\d-]+)[^\d-]+([\d-]+)[^\d-]+([\d-]+)"
-	robots = Vector{Int}[]
-	for line in readlines("$DIR/day14.txt")
-		push!(robots, parse.(Int, match(regex, line)))
-	end
-	x_size, y_size = 101, 103
-	quad_counts = [0, 0, 0, 0]
-    quad_indices = zeros(Int8, x_size, y_size)
-    quad_indices[52:x_size, 1:51] .= 1
-    quad_indices[1:50, 1:51] .= 2
-    quad_indices[1:50, 53:y_size] .= 3
-    quad_indices[52:x_size, 53:y_size] .= 4
+    regex = r"[^\d-]+([\d-]+)[^\d-]+([\d-]+)[^\d-]+([\d-]+)[^\d-]+([\d-]+)"
+    robots = Vector{Int}[]
+    for line in readlines("$DIR/day14.txt")
+        push!(robots, parse.(Int, match(regex, line)))
+    end
+    x_size, y_size = 101, 103       
+    quad_counts = [0, 0, 0, 0]
 
-	for _ in 1:100
-		for r in robots
-			r[1] = mod(r[1] + r[3], x_size)
-            r[2] = mod(r[2] + r[4], y_size)
-		end
-	end
-	for r in robots
-        if (idx = quad_indices[r[1] + 1, r[2] + 1]) > 0
-            quad_counts[idx] += 1
+    for r in robots
+        x, y = mod(r[1] + 100 * r[3], x_size), mod(r[2] + 100 * r[4], y_size)
+        if x < 50
+            if y < 51
+                quad_counts[2] += 1
+            elseif y > 51
+                quad_counts[3] += 1
+            end
+        elseif x > 50
+            if y < 51
+                quad_counts[1] += 1
+            elseif y > 51
+                quad_counts[4] += 1
+            end
         end
-	end
-	part[1] = prod(quad_counts)
+    end
+    part[1] = prod(quad_counts)
 
-	for i in 101:100_000
-		for r in robots
-			r[1] = mod(r[1] + r[3], x_size)
+    col_sums, row_sums = zeros(Int, x_size), zeros(Int, y_size)
+    for i in 1:100_000
+        for r in robots
+            r[1] = mod(r[1] + r[3], x_size)
             r[2] = mod(r[2] + r[4], y_size)
-		end
-		if count(r -> r[1] == 15, robots) > 30 && count(r -> r[2] == 34, robots) > 25 # frame
-			part[2] = i
+            col_sums[r[1] + 1] += 1
+            row_sums[r[2] + 1] += 1
+        end
+        if any(>(30), col_sums) && any(>(30), row_sums)
+            part[2] = i
             break
-		end
-	end
+        end
+        col_sums .= 0
+        row_sums .= 0
+    end
 
-	return part
+    return part
 end
 
 @show day14() #  [223020000, 7338]
