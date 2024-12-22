a5463cba3798b9bf41830e17cfe8659a3adddc01
Bill <wherrera10@users.noreply.github.com>
2024-12-22 12:07:47 -1000

    Update day22.jl

--- src/day22.jl
+++ src/day22.jl
@@ -1,45 +1,40 @@
-using BenchmarkTools
-
 const DIR = "aoc_2024"
 
-next22(n) = (a = ((n * 64) ⊻ n) % 16777216; b = ((a ÷ 32) ⊻ a) % 16777216; ((b * 2048) ⊻ b) % 16777216)
-bananas22(n, k) = (a = [n % 10]; for _ in 1:k push!(a, n % 10); n = next22(n) end; (a, diff(a)))
+function next22(n)
+    a = ((n * 64) ⊻ n) % 16777216
+    b = ((a ÷ 32) ⊻ a) % 16777216
+    return ((b * 2048) ⊻ b) % 16777216
+end
 
 function day22()
     part = [0, 0]
     numbers = parse.(Int, split(read("$DIR/day22.txt", String), r"\s+"))
-    len = length(numbers)
-    run_length, diff_length = 2000, 1999
-
-    for i in numbers
-        for _ in 1:run_length
-            i = next22(i)
-        end
-        part[1] += i
-    end
-
     all_diff = Int[]
-    sequences, diffs = Vector{Vector{Int}}(), Vector{Vector{Int}}()
-    for n in numbers
-        s, d = bananas22(n, run_length)
-        push!(sequences, s)
-        push!(diffs, d)
-        append!(all_diff, d)
+    sequences, diffs = [Int[] for _ in eachindex(numbers)], [Int[] for _ in eachindex(numbers)]
+    n = 0
+    for i in eachindex(numbers)
+        n = numbers[i]
+        push!(sequences[i], n % 10)
+        for _ in 1:2000
+            n = next22(n)
+            push!(sequences[i], n % 10)
+        end
+        part[1] += n
+        diffs[i] = diff(sequences[i])
+        append!(all_diff, diffs[i])
         unique!(all_diff)
     end
 
-    bas = maximum(abs(x) for x in extrema(all_diff)) * 2 + 1
-    @assert bas == 19 # base for digit span of diff
-    sums = zeros(Int, bas^4)
-    @Threads.threads for i in eachindex(sequences)
-        for a in 0:bas^4-1
-            v = digits(a, base = bas, pad = 4) .- (bas ÷ 2)
-            pos = findfirst(j -> diffs[i][j:j+3] == v, 1:diff_length-3)
+    sums = zeros(Int, 19^4)
+    Threads.@threads for i in eachindex(sequences)
+        for a in 0:19^4-1
+            v = digits(a, base = 19, pad = 4) .- 9
+            pos = findfirst(j -> all(v .== @view diffs[i][j:j+3]), 1:1997)
             isnothing(pos) && continue
             sums[a] += sequences[i][pos+4]
         end
     end
-    part[2] =  maximum(sums)
+    part[2] = maximum(sums)
 
     return part
 end
