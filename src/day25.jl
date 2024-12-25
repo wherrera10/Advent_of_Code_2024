fdd5c0e9d60e495d75b08f296abb0f70751b7c5b
Bill <wherrera10@users.noreply.github.com>
2024-12-25 10:27:04 -1000

    Update day25.jl

--- src/day25.jl
+++ src/day25.jl
@@ -2,28 +2,15 @@
 const DIR = "aoc_2024"
 
 function day25()
-    part = [0, 0]
-
     tops, bottoms = Matrix{Char}[], Matrix{Char}[]
     for s in split(read("$DIR/day25.txt", String), "\n\n")
-        push!((s[1:3] == "###" ? tops : bottoms), stack([collect(line) for line in split(s, "\n")], dims = 1))
+        push!((s[1] == '#' ? tops : bottoms), stack([collect(line) for line in split(s, "\n")], dims = 1))
     end
     rows, cols = size(tops[1])
 
-    tcols, bcols = Vector{Vector{Int}}(), Vector{Vector{Int}}()
-    for t in tops
-        push!(tcols, [count(==('#'), @view t[:, i]) for i in 1:cols])
-    end
-    for t in bottoms
-        push!(bcols, [count(==('#'), @view t[:, i]) for i in 1:cols])
-    end   
-    for t in tcols, b in bcols
-        if all(col -> t[col] + b[col] <= rows, 1:cols)
-            part[1] += 1
-        end
-    end
-
-    return part 
+    key_sums = [[count(==('#'), @view t[:, i]) for i in 1:cols] for t in tops]
+    lock_sums = [[count(==('#'), @view t[:, i]) for i in 1:cols] for t in bottoms]
+    return count(all(c -> t[c] + b[c] <= rows, 1:cols) for t in key_sums, b in lock_sums)
 end
 
 @show day25() [2978, 0]
