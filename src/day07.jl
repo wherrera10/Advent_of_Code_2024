96f48acdb8e9fa858b4306773495c2face89cdef
Bill <wherrera10@users.noreply.github.com>
2024-12-28 13:04:33 -1000

    Update day07.jl

--- src/day07.jl
+++ src/day07.jl
@@ -1,58 +1,26 @@
 const DIR = "aoc_2024"
 
+function dfs07(remaining, factors, target, part)
+    remaining == 0 && return target == 0
+    target < 0 && return false
+    factor = factors[remaining]
+    target % factor == 0 && dfs07(remaining - 1, factors, target ÷ factor, part) && return true
+    part == 1 && return dfs07(remaining - 1, factors, target - factor, part)
+    p10 = 10^ndigits(factor)
+    target % p10 == factor && dfs07(remaining - 1, factors, target ÷ p10, part) && return true
+    return dfs07(remaining - 1, factors, target - factor, part)
+end
 function day07()
     part = [0, 0]
-    loc = ReentrantLock()
-
-    @Threads.threads for line in readlines("$DIR/day07.txt")
+    for line in readlines("$DIR/day07.txt")
         a = parse.(Int, split(line, r"\D+"))
         net, factors = a[1], a[2:end]
-        len = length(factors) - 1
-        result = 0
-        for i in 0:2^len-1
-            result = factors[1]
-            j = 2
-            for dig in digits(i, base=2, pad=len)
-                if dig == 0
-                    result += factors[j]
-                else
-                    result *= factors[j]
-                end
-                j += 1
-                result > net && break
-            end
-            if net == result
-                lock(loc)
-                part[1] += net
-                unlock(loc)
-                break
-            end
-        end
-
-        for i in 0:3^len-1
-            result = factors[1]
-            j = 2
-            for dig in digits(i, base=3, pad=len)
-                if dig == 0
-                    result += factors[j]
-                elseif dig == 1
-                    result *= factors[j]
-                else # ||
-                    result = result * 10^ndigits(factors[j]) + factors[j]
-                end
-                j += 1
-                result > net && break
-            end
-            if net == result
-                lock(loc)
-                part[2] += net
-                unlock(loc)
-                break
-            end
-        end    
+        part[1] += dfs07(length(factors), factors, net, 1) ? net : 0
+        net, factors = a[1], a[2:end]
+        part[2] += dfs07(length(factors), factors, net, 2) ? net : 0
     end
 
-    return part 
+    return part
 end
 
 @show day07() #  [28730327770375, 424977609625985]
