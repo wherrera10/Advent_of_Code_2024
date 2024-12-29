93b7433d7b14a4ba434f1277db94f74d338ee4cb
Bill <wherrera10@users.noreply.github.com>
2024-12-29 11:53:38 -1000

    Update day18.jl

--- src/day18.jl
+++ src/day18.jl
@@ -1,58 +1,60 @@
-using Graphs
-
 const DIR = "aoc_2024"
 
-scalar18(c, n) = (c[1] - 1) * n + c[2]
-scalar18(x, y, n) = (x - 1) * n + y
-
+const DIRECTIONS18 = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+const MATRIX_SIZE18 = 71
+function bfs18(mat)
+    moves = [[0, 1, 1]]
+    visited = Set{Vector{Int}}()
+    while !isempty(moves)
+        sort!(moves)
+        dist, x, y = popfirst!(moves)
+        if [x, y] ∉ visited
+            push!(visited, [x, y])
+            if x == MATRIX_SIZE18 && y == MATRIX_SIZE18
+                return dist
+            end
+            for (dx, dy) in DIRECTIONS18
+                x2, y2 = x + dx, y + dy
+                if 1 <= x2 <= MATRIX_SIZE18 && 1 <= y2 <= MATRIX_SIZE18 && mat[x2, y2] == 0 && [x2, y2] ∉ visited
+                    push!(moves, [dist + 1, x2, y2])
+                end
+            end
+        end
+    end
+    return -1 # fail
+end
 function day18()
-    part = [0, Tuple{Int}]
-    directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+    part = [0, String]
     numbers = parse.(Int, split(read("$DIR/day18.txt", String), r"\D+"))
     bytes = [[numbers[i-1] + 1, numbers[i] + 1] for i in 2:2:length(numbers)]
-    rows, cols = 71, 71
-
-
+    rows, cols = MATRIX_SIZE18, MATRIX_SIZE18
     grid = zeros(Int8, rows, cols)
-    g = SimpleGraph{Int32}(rows * cols)
-    start_c = [1, 1]
-    stop_c = [rows, cols]
-    start_vertex = scalar18(start_c, cols)
-    stop_vertex = scalar18(stop_c, cols)
-    for p in @view bytes[1:1024]
-        grid[p[1], p[2]] += 1
+    for (x, y) in @view bytes[1:1024]
+        grid[x, y] = 1
     end
-    for c in CartesianIndices(grid)
-        grid[c] > 0 && continue
-        x, y = c[1], c[2]
-        for d in directions
-            x2, y2 = x + d[1], y + d[2]
-            if 1 <= x2 <= rows && 1 <= y2 <= cols && grid[x2, y2] == 0
-                add_edge!(g, scalar18(c, cols), scalar18(x2, y2, cols))
+
+    part[1] = bfs18(grid)
+
+    mat = copy(grid)
+    left, right = 1025, length(bytes)
+    while left < right
+        mid = (right + left) ÷ 2
+        for i in left:mid
+            x, y = bytes[i]
+            mat[x, y] = 1
+        end
+        if bfs18(mat) > 0
+            left = mid + 1
+        else
+            right = mid
+            mat = copy(grid)
+            for i in 1:left
+                x, y = bytes[i]
+                mat[x, y] = 1
             end
         end
     end
-
-    path = a_star(g, start_vertex, stop_vertex)
-    part[1] = length(path)
-
-    for p in @view bytes[1025:end]
-        x, y = p[1], p[2]
-        grid[x, y] > 0 && continue
-        grid[x, y] += 1
-        for d in directions
-            x2, y2 = x + d[1], y + d[2]
-            if 1 <= x2 <= rows && 1 <= y2 <= cols && grid[x2, y2] == 0
-                rem_edge!(g, scalar18(x2, y2, cols), scalar18(x, y, cols))
-                rem_edge!(g, scalar18(x, y, cols), scalar18(x2, y2, cols))
-            end
-        end
-        path = a_star(g, start_vertex, stop_vertex)
-        if length(path) < 1
-            part[2] = (x - 1, y - 1)
-            break
-        end
-    end
+    part[2] = "$(bytes[left][1]-1),$(bytes[left][2]-1)"
 
     return part
 end
