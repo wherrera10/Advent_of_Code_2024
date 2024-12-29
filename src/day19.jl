b09ddd4b5574ebf330c22f57ba4096f511652fe2
Bill <wherrera10@users.noreply.github.com>
2024-12-29 13:18:50 -1000

    Update day19.jl

--- src/day19.jl
+++ src/day19.jl
@@ -2,14 +2,23 @@ using Memoization
 
 const DIR = "aoc_2024"
 
-@memoize can19(s, a) = any(p == s || startswith(s, p) && can19(s[length(p)+1:end], a) for p in a)
-
 @memoize all19(s, a) = sum((p == s) + (startswith(s, p) ? (all19(s[length(p)+1:end], a)) : 0) for p in a)
 
 function day19()
     text1, text2 = split(read("$DIR/day19.txt", String), "\n\n")
     patterns, desired = split(text1, r"\s?,\s?"), split(text2, r"\s+")
-    count(can19(d, patterns) for d in desired), sum(all19(d, patterns) for d in desired)
+    locker = ReentrantLock()
+    n_can, n_all = 0, 0
+    @Threads.threads for s in desired
+        k = all19(s, patterns)
+        if k > 0
+            lock(locker)
+            n_can += 1
+            n_all += k 
+            unlock(locker)
+        end
+    end
+    n_can, n_all
 end
 
 @show day19() #  [358, 600639829400603]
